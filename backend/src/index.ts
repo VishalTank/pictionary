2fc09de40aeab09da7cba2a6a7005df93afd586b
Vishal Tank <tankvishal19101997@gmail.com>
2020-05-11 01:10:40 +0530

    converted the HTTP server to an HTTPS server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,25 @@
 require('dotenv').config();
 import express from 'express';
+import https from 'https';
+import path from 'path';
 import { server } from './server';
 import { AppConfig } from './utils/app.config';
 import { logger } from './services/app.logger';
+import fs from 'fs';
 
 class App {
 	private app: express.Application = server.getServer();
 
+	private key: Buffer = fs.readFileSync(path.normalize('./keys/server.key'));
+	private cert: Buffer = fs.readFileSync(path.normalize('./keys/server.cert'));
+	private serverOptions = { key: this.key, cert: this.cert };
+
 	public initializeHttpServer(): void {
-		this.app.listen(AppConfig.server.port, '0.0.0.0', () => {
-			this.log(`HTTP Server started listening on: ${AppConfig.server.port}`);
-		});
+		https
+			.createServer(this.serverOptions, this.app)
+			.listen(AppConfig.server.port, '0.0.0.0', () => {
+				this.log(`HTTPS Server started listening on: ${AppConfig.server.port}`);
+			});
 	}
 
 	private log(message: string): void {
