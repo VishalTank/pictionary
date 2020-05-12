262fb1c123af6af4606dec21a1b157df70cbaaa6
Vishal Tank <tankvishal19101997@gmail.com>
2020-05-12 22:49:20 +0530

    added socket server implementation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 require('dotenv').config();
 import express from 'express';
+import http from 'http';
 import https from 'https';
 import path from 'path';
+import socketIO from 'socket.io';
 import { server } from './server';
 import { AppConfig } from './utils/app.config';
 import { logger } from './services/app.logger';
@@ -9,13 +11,13 @@ import fs from 'fs';
 
 class App {
 	private app: express.Application = server.getServer();
+	private httpServer: http.Server;
+	private io: socketIO.Server;
 
 	public initialize(): void {
 		this.initializeHttpServer()
-			.then(() => { })
-			.catch(err => {
-				console.log(err);
-			});
+			.then(() => this.initializeSocketIOServer())
+			.catch(err => console.log(err));
 
 		//? OPTIONAL
 		// this.initializeHttpsServer();
@@ -24,11 +26,31 @@ class App {
 	private initializeHttpServer(): Promise<void> {
 
 		return new Promise((resolve, reject) => {
-			this.app
-				.listen(AppConfig.server.port, '0.0.0.0', () => {
-					logger.info(`HTTP Server started listening on: ${AppConfig.server.port}`);
-					return resolve();
+			this.httpServer = http.createServer(this.app);
+
+			this.httpServer.listen(AppConfig.server.port, AppConfig.server.host, () => {
+				this.log(`HTTP Server started listening on: ${AppConfig.server.port}`);
+				resolve();
+			});
+		});
+	}
+
+	private initializeSocketIOServer(): Promise<void> {
+		return new Promise((resolve, reject) => {
+			this.io = socketIO(this.httpServer);
+
+			this.io.on('connection', (socket) => {
+				this.log('A user has connected to the socket!');
+
+				socket.on('message', (message) => {
+					this.io.emit('message', message);
 				});
+
+				socket.on('disconnect', () => {
+					this.log('A user was disconnected from the socket.');
+				});
+			});
+			resolve();
 		});
 	}
 
