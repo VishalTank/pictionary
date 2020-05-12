96e5b0c9f68fc94f7b15e7492cb92c653021f652
Vishal Tank <tankvishal19101997@gmail.com>
2020-05-12 22:46:38 +0530

    renamed frontend to frontend-react

--- frontend/src/ChatContext.ts
+++
@@ -1,7 +0,0 @@
-import React, { createContext, useContext } from 'react';
-import { SocketService } from './services/SocketService';
-
-export const ChatContext: React.Context<SocketService> = createContext(new SocketService());
-
-// functional component context hook
-export const useChat = () => useContext(ChatContext);
